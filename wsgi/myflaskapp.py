77223f2cbba9e00679347fbff8d37c02849b3277
paoloT <paolo.tomelleri@gmail.com>
2016-05-10 23:19:55 +0200

    cambiata porta dad 4001 a 80 per vedere se è un errore su openshift

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,32 @@
 from flask import Flask
+
+#---
+from flask import request, send_from_directory
+from flask import render_template
+from flask import jsonify
+from flask.ext.cors import CORS
+
+CORS(app)
+#---
+
+
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+
+#---
+@app.route("/index.html")
+def hello1():
+    return send_from_directory('.', 'index.html')
+
+#---
 
 if __name__ == "__main__":
     app.run()
